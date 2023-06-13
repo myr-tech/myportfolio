e9d1d2ca92501727eedb8e81844637136f87aacd
myr-tech <mayur.surbhalavi@gmail.com>
2023-06-13 14:39:32 +0530

    updated

---
+++ script.js
@@ -0,0 +1,5 @@
+// const menuclickbutton=document.getElementById('menuclickbutton');
+// const menushow=document.getElementById('menu');
+// menuclickbutton.addEventListner('click',()=>{
+//     menushow.classlist.toggle('active');
+// })
