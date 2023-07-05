58dfde2655de9e17e1e44b1a53f946f1265ca903
myr-tech <mayur.surbhalavi@gmail.com>
2023-07-05 16:09:35 +0530

    New Ui
    whole website Ui changed

--- script.js
+++
@@ -1,5 +0,0 @@
-// const menuclickbutton=document.getElementById('menuclickbutton');
-// const menushow=document.getElementById('menu');
-// menuclickbutton.addEventListner('click',()=>{
-//     menushow.classlist.toggle('active');
-// })
